0712e7481b7977b6d39627f1c04cf5f42e3d6140
zdang <1137729123@qq.com>
2022-01-19 10:04:27 +0800

    project

---
+++ spring-demo-all/src/main/java/com/example/springdemoall/SpringDemoAllApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdemoall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoAllApplication.class, args);
+	}
+
+}
---
+++ spring-demo-all/src/test/java/com/example/springdemoall/SpringDemoAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemoall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
